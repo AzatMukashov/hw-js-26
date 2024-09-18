aa0fc34fabf60fce75a702cf82d340cd96ff430e
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-09-19 01:03:54 +0600

    переименовал классы тут тоже

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -8,16 +8,14 @@ interface TaskProps {
 
 const Task: React.FC<TaskProps> = ({id, text, onDelete}) => {
     return (
-        <div className="task-form">
-            <div className="task-form__form">
-                <span className="task-form__text">{text}</span>
-                <button
-                    className="task-form__submit"
-                    type="button"
-                    onClick={() => onDelete(id)}
-                    aria-label="delete task">delete
-                </button>
-            </div>
+        <div className="task">
+            <span className="task__text">{text}</span>
+            <button
+                className="task__delete"
+                type="button"
+                onClick={() => onDelete(id)}
+                aria-label="delete task">delete
+            </button>
         </div>
     );
 };
