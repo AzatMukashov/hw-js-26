2b570c258709a0aa67e616351ba89be32c6633ad
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-09-19 00:55:43 +0600

    импортировал нужные компоненты, определил интерфейс, добавил дефолтные задачи, добавил создание задач с уникальным айди, добавил функцию удаления задач по айди, нарисовал разметку для будущей ее стилизации где вызываются компоненты с их данными которые итерируются и функциями

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import AddTaskForm from "./components/AddTaskForm.tsx";
+import Task from "./components/Task.tsx";
+
+interface Props {
+    id: string;
+    text: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [tasks, setTasks] = useState<Props[]>([
+        {text: 'Buy milk', id: '123'},
+        {text: 'Walk with dog', id: '234'},
+        {text: 'Do homework', id: '345'},
+    ]);
+    const addTask = (text: string) => {
+        const newTask = {
+            id: Date.now().toString(), text,
+        };
+        setTasks([...tasks, newTask]);
+    };
+    const deleteTask = (id: string) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+    return (
+        <div className="app">
+            <AddTaskForm onAddTask={addTask}/>
+            <div className="task-list">
+                {tasks.map((task) => (
+                    <Task key={task.id} id={task.id} text={task.text} onDelete={deleteTask}/>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default App
